384abf353738501a66f7158005f073ff6d39c7ae
adjita <adjambengue986@icloud.com>
2024-02-08 14:16:57 +0100

    Affichage de l'année de naissance de l'utilisateur

--- info_utilisateur.py
+++ info_utilisateur.py
@@ -1,7 +1,9 @@
 prenom = input("Entrez votre prénom : ")
 nom = input("Entrez votre nom : ")
 age = input("Entrez votre âge : ")
+
 annee_naissance = 2024 - int(age)
+
 print("Bonjour,", prenom, nom)
 print("Vous avez", age, "ans.")
-print("Vous êtes né(e) en", annee_naissance)
+print("Vous êtes née en", annee_naissance, "ans merci")
